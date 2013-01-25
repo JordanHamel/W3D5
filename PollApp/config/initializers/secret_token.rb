e9c385376e3ce88e4e75945a2b0f50a4d37b56e7
Erik Trautman <erik.trautman@gmail.com>
2013-01-25 09:30:06 -0800

    initialized rails project

---
+++ PollApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollApp::Application.config.secret_token = 'e85688b3faa89a10cf5c188078af2b7ea7b1d7316ec2ad4feb0f8d7cbab4031eeff3b6e5c1025e68e3d642ba512b7d218c8b49bd97758a4c3a2a94bd4ba134b8'
